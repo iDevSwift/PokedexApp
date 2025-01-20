237420f94d0bcebc41bb9db5c381d2caf0b63cab
Diego Maseda Fdez <idevyswift24@gmail.com>
2025-01-20 20:51:01 +0100

    Initial Commit

---
+++ PokedexApp/PokedexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppApp.swift
+//  PokedexApp
+//
+//  Created by Diego Maseda on 20/1/25.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
